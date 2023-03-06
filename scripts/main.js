0afc585d83c89c12246442bb731c73fce11045b7
necoop <amok2005@gmail.com>
2023-03-07 00:06:50 +0700

    alpha 0.1

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+//Скрытие панели навигации при прокрутке вниз
+// let lastScrollTop = 0;
+// $(window).scroll(function(event) {
+//   let st = $(this).scrollTop();
+//   if (st > lastScrollTop) {
+//     // scroll down
+//     $('.fixed-top').addClass('navbar-hidden');
+//     $('.fixed-top').removeClass('navbar-visible');
+//   } else {
+//     // scroll up
+//     $('.fixed-top').addClass('navbar-visible');
+//     $('.fixed-top').removeClass('navbar-hidden');
+//   }
+//   lastScrollTop = st;
+// });
