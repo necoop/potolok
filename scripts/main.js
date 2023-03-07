fef94bcf4e0ffe404e83dc7e588cd0a506b848ba
necoop <amok2005@gmail.com>
2023-03-07 20:04:49 +0700

    Slider

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,8 @@
-let menuToggle = $("#menu__toggle");
-menuToggle.change((e) => {
-  if (menuToggle[0].checked) {
-    $("body").addClass("noscrloll");
-  } else {
-    $("body").removeClass("noscrloll");
-  }
-});
+  let menuToggle = $("#menu__toggle");
+  menuToggle.change((e) => {
+    if (menuToggle[0].checked) {
+      $("body").addClass("noscrloll");
+    } else {
+      $("body").removeClass("noscrloll");
+    }
+  });
---
+++ scripts/slider.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+    let newSlidesPerView;
+      $(window).on("load", resizeSlider);
+      $(window).on("resize", resizeSlider);
+      
+      
+      function resizeSlider(){
+        let bodyWidth = $("body").width();
+        if (bodyWidth < 550) {
+            newSlidesPerView = 2.1;
+            swiper.params.slidesPerView = newSlidesPerView;
+            swiper.update();
+          }
+        if (bodyWidth < 500) {
+          newSlidesPerView = 1.9;
+          swiper.params.slidesPerView = newSlidesPerView;
+          swiper.update();
+        }
+        if (bodyWidth < 400) {
+          newSlidesPerView = 1.8;
+          swiper.params.slidesPerView = newSlidesPerView;
+          swiper.update();
+        }
+        if (bodyWidth < 370) {
+          newSlidesPerView = 1.6;
+          swiper.params.slidesPerView = newSlidesPerView;
+          swiper.update();
+        }
+        if (bodyWidth < 340) {
+          newSlidesPerView = 1.4;
+          swiper.params.slidesPerView = newSlidesPerView;
+          swiper.update();
+        }
+        swiper.params.slidesPerView = newSlidesPerView;
+        swiper.update();
+      };
+    
+    const swiper = new Swiper(".swiper", {
+        // Optional parameters
+        direction: "horizontal",
+        centeredSlides: true,
+        slidesPerView: 1.7,
+        spaceBetween: 15,
+        loop: true,
+      });
+      
+      
+  });
