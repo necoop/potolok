1f78805563ffc2404cacd923f02de7b3322a0441
necoop <amok2005@gmail.com>
2023-03-07 16:55:52 +0700

    карточки с преимуществами

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,3 @@
-//Скрытие панели навигации при прокрутке вниз
-// let lastScrollTop = 0;
-// $(window).scroll(function(event) {
-//   let st = $(this).scrollTop();
-//   if (st > lastScrollTop) {
-//     // scroll down
-//     $('.fixed-top').addClass('navbar-hidden');
-//     $('.fixed-top').removeClass('navbar-visible');
-//   } else {
-//     // scroll up
-//     $('.fixed-top').addClass('navbar-visible');
-//     $('.fixed-top').removeClass('navbar-hidden');
-//   }
-//   lastScrollTop = st;
-// });
 let menuToggle = $("#menu__toggle");
 menuToggle.change((e) => {
   if (menuToggle[0].checked) {
